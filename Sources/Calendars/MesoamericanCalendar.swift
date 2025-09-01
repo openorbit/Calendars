1937a548717204e0bb9b2167877b8c6ead396b86
Mattias Holm <lorrden@openorbit.org>
2025-09-01 06:17:43 +0200

    Adjust protection levels of API

--- Sources/Calendars/MesoamericanCalendar.swift
+++ Sources/Calendars/MesoamericanCalendar.swift
@@ -19,17 +19,17 @@
 import Foundation
 
 public struct MesoamericanDate : Equatable {
-  var hablatun: Int = 0
-  var alautun: Int = 0
-  var kinchiltun: Int = 0
-  var kalabtun: Int = 0
-  var piktun: Int = 0
-  var baktun: Int
-  var katun: Int
-  var tun: Int
-  var winal: Int
-  var kin: Int
-  init() {
+  public var hablatun: Int = 0
+  public var alautun: Int = 0
+  public var kinchiltun: Int = 0
+  public var kalabtun: Int = 0
+  public var piktun: Int = 0
+  public var baktun: Int
+  public var katun: Int
+  public var tun: Int
+  public var winal: Int
+  public var kin: Int
+  public init() {
     hablatun = 0
     alautun = 0
     kinchiltun = 0
@@ -42,7 +42,7 @@ public struct MesoamericanDate : Equatable {
     kin = 0
   }
 
-  init(comps: [Int]) {
+  public init(comps: [Int]) {
     self.init()
     if comps.count == 5 {
       baktun = comps[0]
@@ -68,36 +68,36 @@ public struct MesoamericanLongCountCalendar {
   static let cycleSize = [460800000000, 23040000000, 1152000000, 57600000,
                           2880000,
                           144000, 7200, 360, 20, 1]
-  
+
   public static func isProleptic(_ d: Int) -> Bool {
     return d < epoch
   }
 
-  static func toJDN(hablatun: Int = 0,
-                    alautun: Int = 0,
-                    kinchiltun: Int = 0,
-                    kalabtun: Int = 0,
-                    piktun: Int = 0,
-                    baktun: Int,
-                    katun: Int,
-                    tun: Int,
-                    winal: Int,
-                    kin: Int) -> Int {
+  public static func toJDN(hablatun: Int = 0,
+                           alautun: Int = 0,
+                           kinchiltun: Int = 0,
+                           kalabtun: Int = 0,
+                           piktun: Int = 0,
+                           baktun: Int,
+                           katun: Int,
+                           tun: Int,
+                           winal: Int,
+                           kin: Int) -> Int {
 
     return hablatun * cycleSize[0] +
-      alautun * cycleSize[1] +
-      kinchiltun * cycleSize[2] +
-      kalabtun * cycleSize[3] +
-      piktun * cycleSize[4] +
-      baktun * cycleSize[5] +
-      katun * cycleSize[6] +
-      tun * cycleSize[7] +
-      winal * cycleSize[8] +
-      kin * cycleSize[9] +
-      gmtCorrection
+    alautun * cycleSize[1] +
+    kinchiltun * cycleSize[2] +
+    kalabtun * cycleSize[3] +
+    piktun * cycleSize[4] +
+    baktun * cycleSize[5] +
+    katun * cycleSize[6] +
+    tun * cycleSize[7] +
+    winal * cycleSize[8] +
+    kin * cycleSize[9] +
+    gmtCorrection
   }
 
-  static func toDate(J: Int) -> MesoamericanDate {
+  public static func toDate(J: Int) -> MesoamericanDate {
     let t = J - gmtCorrection
     var date = MesoamericanDate()
     date.hablatun = (t / 460800000000) % 20
@@ -114,7 +114,7 @@ public struct MesoamericanLongCountCalendar {
     return date
   }
 
-  static func isValidDate(d: [Int]) -> Bool {
+  public static func isValidDate(d: [Int]) -> Bool {
     for i in d {
       if i < 0 || 20 <= i {
         return false
