56f1141cf455a3a0cab49bc53892595a84229c8c
Mattias Holm <lorrden@openorbit.org>
2025-09-26 07:17:08 +0200

    Rework calednar registry so it can be used from non-async contexts

--- Sources/Calendars/CalendarRegistry.swift
+++ Sources/Calendars/CalendarRegistry.swift
@@ -1,37 +1,59 @@
 //
-//  CalendarRegistry.swift
-//  Calendars
+// SPDX-License-Identifier: Apache-2.0
 //
-//  Created by Mattias Holm on 2025-09-07.
+// Copyright 2024 Mattias Holm
 //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 import Foundation
 
-public actor CalendarRegistry {
+public struct CalendarRegistry : Sendable {
   public static let shared = CalendarRegistry()
-  private var map: [String: CalendarProtocol] = [:]
-
-  public init() {
-    // Actor inits are nonisolated. Directly initialize stored state instead of
-    // calling an actor-isolated method.
-    map[BahaiCalendar.shared.calendarKey.lowercased()] = BahaiCalendar.shared
-    map[CivilIslamicCalendar.shared.calendarKey.lowercased()] = CivilIslamicCalendar.shared
-    map[CopticCalendar.shared.calendarKey.lowercased()] = CopticCalendar.shared
-    map[EgyptianCalendar.shared.calendarKey.lowercased()] = EgyptianCalendar.shared
-    map[EthiopianCalendar.shared.calendarKey.lowercased()] = EthiopianCalendar.shared
-    map[FrenchRepublicanCalendar.shared.calendarKey.lowercased()] = FrenchRepublicanCalendar.shared
-    map[GregorianCalendar.shared.calendarKey.lowercased()] = GregorianCalendar.shared
-    map[JewishCalendar.shared.calendarKey.lowercased()] = JewishCalendar.shared
-    map[JulianCalendar.shared.calendarKey.lowercased()] = JulianCalendar.shared
-    map[RomanCalendar.shared.calendarKey.lowercased()] = RomanCalendar.shared
-    map[SakaCalendar.shared.calendarKey.lowercased()] = SakaCalendar.shared
-    map[SwedishCalendar.shared.calendarKey.lowercased()] = SwedishCalendar.shared
-  }
-
-  public func register(_ cal: CalendarProtocol) {
-    map[cal.calendarKey.lowercased()] = cal
-  }
+  private var map: [String: CalendarProtocol] = [
+    BahaiCalendar.shared.calendarKey.lowercased() : BahaiCalendar.shared,
+    CivilIslamicCalendar.shared.calendarKey.lowercased() : CivilIslamicCalendar.shared,
+    CopticCalendar.shared.calendarKey.lowercased() : CopticCalendar.shared,
+    EgyptianCalendar.shared.calendarKey.lowercased() : EgyptianCalendar.shared,
+    EthiopianCalendar.shared.calendarKey.lowercased() : EthiopianCalendar.shared,
+    FrenchRepublicanCalendar.shared.calendarKey.lowercased() : FrenchRepublicanCalendar.shared,
+    GregorianCalendar.shared.calendarKey.lowercased() : GregorianCalendar.shared,
+    JewishCalendar.shared.calendarKey.lowercased() : JewishCalendar.shared,
+    JulianCalendar.shared.calendarKey.lowercased() : JulianCalendar.shared,
+    RomanCalendar.shared.calendarKey.lowercased() : RomanCalendar.shared,
+    SakaCalendar.shared.calendarKey.lowercased() : SakaCalendar.shared,
+    SwedishCalendar.shared.calendarKey.lowercased() : SwedishCalendar.shared
+  ]
+  private var idMap: [String: CalendarId] = [
+    BahaiCalendar.shared.calendarKey.lowercased() : .bahai,
+    CivilIslamicCalendar.shared.calendarKey.lowercased() : .civilIslamic,
+    CopticCalendar.shared.calendarKey.lowercased() : .coptic,
+    EgyptianCalendar.shared.calendarKey.lowercased() : .egyptian,
+    EthiopianCalendar.shared.calendarKey.lowercased() : .ethiopian,
+    FrenchRepublicanCalendar.shared.calendarKey.lowercased() : .frenchRepublican,
+    GregorianCalendar.shared.calendarKey.lowercased() : .gregorian,
+    JewishCalendar.shared.calendarKey.lowercased() : .jewish,
+    JulianCalendar.shared.calendarKey.lowercased() : .julian,
+    RomanCalendar.shared.calendarKey.lowercased() : .romanRepublican,
+    SakaCalendar.shared.calendarKey.lowercased() : .saka,
+    SwedishCalendar.shared.calendarKey.lowercased() : .swedish
+  ]
 
   public func calendar(for id: String) -> CalendarProtocol? {
     return map[id.lowercased()]
   }
+  public func calendarID(for id: String) -> CalendarId? {
+    return idMap[id.lowercased()]
+  }
+
 }
